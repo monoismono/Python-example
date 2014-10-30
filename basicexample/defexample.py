0076ff22d4f9c5ab7b9dd53800089764d95315c7
Mono Lu <holgaweb@gmail.com>
2014-10-31 00:49:47 +0800

    show how to define a function

---
+++ basicexample/defexample.py
@@ -0,0 +1,35 @@
+# Code example create by mono
+# 2014.10.30
+# this py is for basic example
+
+import os
+
+print( 'this is example for how to def()\n' )
+
+def str_test( name ) :
+    if name == "mono" :
+       print( 'Hi mono' )
+    else :
+       print( 'Who are you?')
+
+def num_test( x , y ) :
+    if x > y :
+       print( str( x ) + " > " + str( y ) )
+    elif x < y :
+       print( str( x ) + " < " + str( y ) )
+    else :
+       print( str( x ) + " = " + str( y ) )
+
+print( 'result of str_test( mono ) is ' )
+str_test( 'mono' )
+print( 'result of str_test( nono ) is ' )
+str_test( 'nono' )
+print( 'result of num_test( 1 , 2 ) is ' )
+num_test( 1 , 2 )
+print( 'result of num_test( 2 , 1 ) is ' )
+num_test( 2 , 1 )
+print( 'result of num_test( 1 , 1 ) is ' )
+num_test( 1 , 1 )
+print( ' ' )
+
+os.system( "pause" )
