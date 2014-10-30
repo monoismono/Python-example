363071b26e912832b6ecaabc586b532c41f1dce8
Mono Lu <holgaweb@gmail.com>
2014-10-31 00:16:31 +0800

    add example of if...else...

---
+++ basicexample/ifelseexample.py
@@ -0,0 +1,26 @@
+# Code example create by mono
+# 2014.10.30
+# this py is for basic example
+
+print( 'This is example for python if...else \n' )
+
+x = 1
+y = 2
+
+if x > y :
+  print( 'x > y is ' + str( x > y ) )
+elif x == y :
+  print( 'x = y is ' + str( x == y ) )
+else :
+  print( 'x < y is ' + str( x < y ) )
+
+print( ' ' )
+
+name = 'mono'
+
+if name == 'mono' :
+  print( 'Hi mono ' )
+elif name == 'nomo' :
+  print( 'Hi nomo ' )
+else :
+  print( 'Hi nono ' )
